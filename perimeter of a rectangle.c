07c0a8d02b7c5708fb29b245dcb65a1df1efe859
Mchepkemoi <91831499+Mchepkemoi@users.noreply.github.com>
2021-10-22 14:54:22 +0300

    Create perimeter of a rectangle.c

---
+++ perimeter of a rectangle.c
@@ -0,0 +1,13 @@
+/*division*/
+#include <stdio.h>
+int main()
+{
+    int c,d,division;
+    printf("enter a value to c\n:");
+    scanf("%d",&c);
+    printf("enter a value to d\n:");
+    scanf("%d",&d);
+    division=c/d;
+    printf("division=%d",division);
+    return 0;
+}
